65e0d96f7edaf237ca88dcbb84685d18466450f6
SAURABH <er.saurabhkumar22@gmail.com>
2026-02-11 05:57:32 +0530

    push

--- src/test/java/PracticeDay1/Loadingpage.java
+++ src/test/java/PracticeDay1/Loadingpage.java
@@ -1,4 +1,23 @@
 package PracticeDay1;
 
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+
 public class Loadingpage {
+
+    public static void main (String[] args) throws InterruptedException {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        BrowserContext context = browser.newContext();
+        Page page = context.newPage();
+        page.navigate("https://google.com");
+        Thread.sleep(5000);
+        page.close();
+        context.close();
+        browser.close();
+        playwright.close();
+    }
+
 }
