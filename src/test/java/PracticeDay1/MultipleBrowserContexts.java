6ded88fb36e110254b6ce9cb18b2ff5de6f311bf
SAURABH <er.saurabhkumar22@gmail.com>
2026-06-09 19:50:02 +0530

    push1

--- src/test/java/PracticeDay1/MultipleBrowserContexts.java
+++ src/test/java/PracticeDay1/MultipleBrowserContexts.java
@@ -1,8 +1,18 @@
 package PracticeDay1;
 
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+
+import java.security.PublicKey;
 
 public class MultipleBrowserContexts {
-    Public_static void main(String[] args) {
-        Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)).newContext(new Browser.NewContextOptions().setViewportSize(1521, 920)).newPage().navigate("https://www.google.com/");   }
+    public static void main(String[] args){
+        Playwright pw = Playwright.create();
+        Browser bw = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext context1 = bw.newContext(new Browser.NewContextOptions().setViewportSize(1521, 920));
+        Page Pw1 = context1.newPage();
+        Pw1.navigate("http://google.com");
+        BrowserContext context2 = bw.newContext(new Browser.NewContextOptions().setViewportSize(1521, 920));
+        Page Pw2 = context2.newPage();
+        Pw2.navigate("http://bing.com");
+           }
 }
