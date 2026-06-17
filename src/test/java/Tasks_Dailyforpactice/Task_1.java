c259b5484b471a145997869b46e368f053da4cf8
SAURABH <er.saurabhkumar22@gmail.com>
2026-06-17 17:57:22 +0530

    push1

--- src/test/java/Tasks_Dailyforpactice/Task_1.java
+++ src/test/java/Tasks_Dailyforpactice/Task_1.java
@@ -1,11 +1,16 @@
 package Tasks_Dailyforpactice;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Playwright;
 
 public class Task_1 {
 
     public static void main(String[] args) {
         Playwright pw=Playwright.create();
+        Browser Bw=pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+         Bw.newContext().newPage().navigate("https://demo.automationtesting.in/Register.html");
+         System.out.println(Bw.newContext().newPage().title());
 
     }
 }
@@ -13,11 +18,11 @@ public class Task_1 {
 /* practice automtion website
      * 1. open the browser
      * 2. navigate to https://demo.automationtesting.in/Register.html
-     * 3. close the browser
+     * 3. maximize the window
      * 4. print the title of the page
-     * 5. maximize the window
-     * 6. capture the screenshot of the page
-     * 7. handle multiple browser contexts
+     * 5. capture the screenshot of the page
+     * 6. handle multiple browser contexts
+     * 7. close the browser
      * 8. locators in playwritght
  Websites to practice:
      * https://demo.automationtesting.in/Register.html
