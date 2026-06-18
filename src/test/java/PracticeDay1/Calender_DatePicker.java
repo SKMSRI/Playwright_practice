86a6089accab5cb4c2fa721f9f833e70ca4e5c6b
SAURABH <er.saurabhkumar22@gmail.com>
2026-06-18 06:04:00 +0530

    push1

---
+++ src/test/java/PracticeDay1/Calender_DatePicker.java
@@ -0,0 +1,15 @@
+package PracticeDay1;
+
+import com.microsoft.playwright.*;
+
+public class Calender_DatePicker {
+
+    public static void main(String[] args) {
+        Playwright pw = Playwright.create();
+        Browser bw = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext ct = bw.newContext(new Browser.NewContextOptions().setViewportSize(1521, 920));
+        Page Pw = ct.newPage();
+        Pw.navigate("https://demo.automationtesting.in/Datepicker.html");
+        //Page.Locator(Selectors ""        )
+    }
+}
