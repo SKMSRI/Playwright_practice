2574cb763fd36776246623d825fbf61f6d6ffb02
SAURABH <er.saurabhkumar22@gmail.com>
2026-06-25 06:03:45 +0530

    push1

--- src/test/java/PracticeDay1/Handling_FileUpload.java
+++ src/test/java/PracticeDay1/Handling_FileUpload.java
@@ -2,8 +2,11 @@ package PracticeDay1;
 
 import com.microsoft.playwright.*;
 import org.junit.Test;
-
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static javax.print.attribute.standard.MediaSizeName.C;
 
 public class Handling_FileUpload {
     @Test
@@ -13,7 +16,20 @@ public class Handling_FileUpload {
         Browser bw = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         BrowserContext ct = bw.newContext(new Browser.NewContextOptions().setViewportSize(1521, 920));
         Page Pw = ct.newPage();
-        Pw.navigate("https://demo.automationtesting.in/FileUpload.html");
-        //Page.setInputFiles("//input[@id='input-4']", Path files = new File("C:\\Users\\Admin\\Downloads\\sampleFile.jpeg"));
-    }
+        Pw.navigate("https://demoqa.com/automation-practice-form");
+
+        //singlefileupload();
+        Pw.setInputFiles("//input[@id=\"uploadPicture\"]",
+                Paths.get("C:/Users/SAURABH/OneDrive/Pictures/Screenshots/1.png"));
+        Thread.sleep(5000);
+        //deattachfile
+        Pw.setInputFiles("//input[@id=\"uploadPicture\"]", new Path[0]);
+
+        //multiple file upload
+//        Pw.setInputFiles("//input[@id=\"uploadPicture\"]",
+//                        new Path[]{
+//                        Paths.get("C:/Users/SAURABH/OneDrive/Pictures/Screenshots/1.png"),
+//                        Paths.get("C:/Users/SAURABH/OneDrive/Pictures/Screenshots/2.png)});
+//
+}
 }
