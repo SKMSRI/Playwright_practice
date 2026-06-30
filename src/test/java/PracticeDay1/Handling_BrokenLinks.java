c0290034b33c5b3bb144d9c4ca3e8e2fb0a13505
SAURABH <er.saurabhkumar22@gmail.com>
2026-07-01 05:21:54 +0530

    push1

--- src/test/java/PracticeDay1/Handling_BrokenLinks.java
+++ src/test/java/PracticeDay1/Handling_BrokenLinks.java
@@ -9,11 +9,21 @@ public class Handling_BrokenLinks {
         BrowserContext ct = bw.newContext(new Browser.NewContextOptions().setViewportSize(1521, 920));
         Page Pw = ct.newPage();
         Pw.navigate("https://demoqa.com/links");
+
         //find all the links on the page
-        Locator links = Pw.locator("//div[@id='linkWrapper']");
+       Locator links = Pw.locator("a");
         int linkCount = links.count();
         System.out.println("Total links on the page: " + linkCount);
 
+        //print the text of each link
+//        for (int i = 0; i < linkCount; i++) {
+//            String linkText = links.nth(i).textContent();
+//            String linkHref = links.nth(i).getAttribute("href");
+//            System.out.println("Link Text: " + linkText + " | Link Href: " + linkHref);
 
-    }
+        //find broken links
+        //
+        }
+
 }
+
---
+++ src/test/java/PracticeDay1/Handling_MultipleWindows_tabs.java
@@ -0,0 +1,39 @@
+package PracticeDay1;
+
+import com.microsoft.playwright.*;
+
+import java.sql.Driver;
+
+public class Handling_MultipleWindows_tabs {
+    public static void main(String[] args) throws InterruptedException {
+        Playwright pw = Playwright.create();
+        Browser bw = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext ct = bw.newContext(new Browser.NewContextOptions().setViewportSize(1521, 920));
+        Page Pw1 = ct.newPage();
+        Pw1.navigate("https://demoqa.com/browser-windows");
+        System.out.println("Parent Window Title: " + Pw1.title());
+
+        //open new tab or window
+        Thread.sleep(5000);
+        Pw1.locator("//button[@id='tabButton']").click();
+        //switch to new tab
+        for (Page newtab : ct.pages()) {
+            if (!newtab.equals(Pw1)) {
+                System.out.println("Switched to New Tab Title: " + newtab.title());
+                break;
+
+            }
+        }
+        //switch to new window
+        Thread.sleep(5000);
+        Pw1.locator("//button[@id='messageWindowButton']").click();
+        for (Page newwindow : ct.pages()) {
+            if (!newwindow.equals(Pw1)) {
+                System.out.println("Switched to New Window Title: " + newwindow.title());
+                break;
+            }
+        }
+
+    }
+}
+//you tube Ur;: https://www.youtube.com/watch?v=qPvmSHffI2A or https://www.youtube.com/watch?v=1MP-14mimPo
