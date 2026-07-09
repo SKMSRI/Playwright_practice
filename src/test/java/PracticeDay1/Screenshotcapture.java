3473352bc6e8d7909deca0019f62753203879a9c
SAURABH <er.saurabhkumar22@gmail.com>
2026-07-09 05:51:51 +0530

    push1

--- src/test/java/PracticeDay1/Screenshotcapture.java
+++ src/test/java/PracticeDay1/Screenshotcapture.java
@@ -1,18 +1,26 @@
 package PracticeDay1;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+
+import java.nio.file.Paths;
 
 public class Screenshotcapture {
-    public static void main(String[] args) {
-       Playwright Pw= Playwright.create();
+    public static void main(String[] args) throws InterruptedException {
+        Playwright pw = Playwright.create();
+        Browser bw = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext ct = bw.newContext(new Browser.NewContextOptions().setViewportSize(1521, 920));
+        Page Pw = ct.newPage();
+        Pw.navigate("https://www.amazon.in/");
+        Thread.sleep(5000);
 
+        //Full Page screenshot
+        Pw.screenshot(new Page.ScreenshotOptions()
+                .setPath(Paths.get("amazon1.png"))
+                .setFullPage(true));
+        //Element screenshot
+        Pw.locator("//img[@alt='Amazon Brand - Solimo']").screenshot(new Locator    .ScreenshotOptions()
+                .setPath(Paths.get("amazon3.png")));
+        bw.close();
 
-      //Browser bw= Pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        //BrowserContext context = bw.newContext(new Browser.NewContextOptions().setViewportSize(1521, 920));
-        //bw.newPage().navigate("https://www.google.com/").screenshot(new Browser.PageScreenshotOptions().setPath("screenshot.png").setFullPage(true));
     }
-
 }
